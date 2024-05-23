4ea498021a7d99f7cd608b9964eda8105359f4f7
AbelLaptop <abel07110711@gmail.com>
2024-05-23 13:54:45 -0500

    interface1

--- Frontend/my-app/src/app/vistas/home/home.component.ts
+++ Frontend/my-app/src/app/vistas/home/home.component.ts
@@ -3,15 +3,18 @@ import {HeaderComponent} from '../../plantillas/header/header.component';
 import {ApiService} from '../../service/api.service';
 import { MyResponse } from '../../service/interfaces';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,CommonModule],
+  imports: [HeaderComponent,CommonModule,FormsModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
+  fechaActual: Date = new Date();
   sintomas: any =  {};
   enfermedades: any =  {};
   errorSintomas = '';
@@ -37,10 +40,25 @@ export class HomeComponent {
     let selectedIndex = $event.target.options[$event.target.options.selectedIndex];
     let text = selectedIndex.text;
     this.valoresSeleccionados['select_'+index] = text;
-    console.log(selectedIndex);
+    console.log('select_'+index);
     console.log(this.valoresSeleccionados);
   }
 
+  onDelete( index:number) {
+    delete this.valoresSeleccionados['select_'+index] ;
+    const i = this.indices.indexOf(index, 0);
+    console.log(this.indices)
+    this.indices.splice(i, 1);
+    console.log(index+" valor " + i);
+    console.log(this.indices)
+  }
+
+  clear(){
+    this.valoresSeleccionados = {};
+    this.indices = [];
+    this.respuesta = null;
+    this.diagnostico = '';
+  }
   getSintomas(){
     this.apiService.getSintomas().subscribe(
       (response:MyResponse) =>{
